a0d54c37a66b5d87a80a51b7bd5006f3884aaf29
alikey121 <alikey135@gmail.com>
2026-03-04 15:33:50 -0500

    funciones.js

--- funciones.js
+++ funciones.js
@@ -4,3 +4,25 @@ function Suma (x){
     };
 
     console.log(Suma(7));
+
+    function Resta (x){
+        let accion = x - 5; 
+        return accion;
+        };
+
+        console.log(Resta(7));        
+
+        function Multiplicacion (x){
+            let accion = x * 5; 
+            return accion;
+            };
+
+            console.log(Multiplicacion(7));
+            
+            function Division (x){
+                let accion = x / 5; 
+                return accion;
+                };
+
+                console.log(Division(7));
+                
