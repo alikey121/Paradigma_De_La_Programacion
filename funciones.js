c83e66b68c182273e726abfd7f349d9560f31b6a
alikey121 <alikey135@gmail.com>
2026-03-04 16:14:04 -0500

    funciones.js

--- funciones.js
+++ funciones.js
@@ -1,28 +1,56 @@
-function Suma (x){
-    let accion = x + 5; 
+function Suma(x) {
+    let accion = x + 5;
     return accion;
-    };
+};
 
-    console.log(Suma(7));
+console.log(Suma(7));
 
-    function Resta (x){
-        let accion = x - 5; 
-        return accion;
-        };
+function Resta(x) {
+    let accion = x - 5;
+    return accion;
+};
 
-        console.log(Resta(7));        
+console.log(Resta(7));
 
-        function Multiplicacion (x){
-            let accion = x * 5; 
-            return accion;
-            };
+function Multiplicacion(x) {
+    let accion = x * 5;
+    return accion;
+};
 
-            console.log(Multiplicacion(7));
-            
-            function Division (x){
-                let accion = x / 5; 
-                return accion;
-                };
+console.log(Multiplicacion(7));
 
-                console.log(Division(7));
-                
+function Division(x) {
+    let accion = x / 5;
+    return accion;
+};
+
+console.log(Division(7));
+
+let ola = 0;
+
+function SumarPrecios(a, b) {
+    return ola = ola + a * b;
+};
+
+console.log(SumarPrecios(4, 6));
+console.log(SumarPrecios(2, 3));
+console.log(SumarPrecios(5, 8));
+
+let resultado = "";
+
+function Sumapalabras(a) {
+    return resultado = resultado + a;
+};
+
+console.log(Sumapalabras("hola "));
+console.log(Sumapalabras("mundo "));
+
+function Multiplicar(a) {
+    return a * a;
+};
+
+function SumaDeLaMultiplicacion(num1, num2) {
+    return Multiplicar(num1) + Multiplicar(num2);
+};
+
+console.log(SumaDeLaMultiplicacion(3, 4));
